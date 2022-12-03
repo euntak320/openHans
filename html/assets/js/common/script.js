27ba31187cf95e61b4425ddd60cd689f85840fa7
hans <hans.han@tosslab.com>
2022-12-03 13:31:07 +0900

    오픈핸즈인 작업 완료

--- html/assets/js/common/script.js
+++ html/assets/js/common/script.js
@@ -2,6 +2,9 @@ $(function () {
 
 	var thisScroll = 20;
 
+	var $tabButton = $('.tab_area .tab_list button')
+	var $tabView = $('.tab_area .view')
+
 	// header
 	const $header = $('header');
 
@@ -14,19 +17,28 @@ $(function () {
 	
 	
 	$(window).scroll(function(e) {
-			scrollTop = $(window).scrollTop();
-			if (scrollTop > thisScroll+5 && scrollTop>5) {
-				$("header").addClass("scroll_menu");
-				$(".main_content").addClass("off")
-			} else {
-				if(scrollTop - thisScroll < 0){
-					$(".main_content").removeClass("off")
-				}
-				if (scrollTop === 0) {
-					$("header").removeClass("scroll_menu");
-				}
+		scrollTop = $(window).scrollTop();
+		if (scrollTop > thisScroll+5 && scrollTop>5) {
+			$("header").addClass("scroll_menu");
+			$(".main_content").addClass("off")
+		} else {
+			if(scrollTop - thisScroll < 0){
+				$(".main_content").removeClass("off")
 			}
-			thisScroll = $(window).scrollTop()
+			if (scrollTop === 0) {
+				$("header").removeClass("scroll_menu");
+			}
+		}
+		thisScroll = $(window).scrollTop()
 	});
+
+	//  tab	
+	$tabButton.click(function(){
+		$tabButton.removeClass('on');
+		$tabButton.eq($(this).index()).addClass('on')
+		$tabView.removeClass('on');
+		$tabView.eq($(this).index()).addClass('on')
+	});	
+
 
 })
